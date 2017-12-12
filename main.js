089133c6cc1bac714d600a320e44c8de40b93797
Sadeesh Radhakrishnan <sadeesh.r@gmail.com>
2017-12-12 10:01:01 +0800

    code push

---
+++ main.js
@@ -0,0 +1,155 @@
+'use strict';
+
+// Import parts of electron to use
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const path = require('path')
+const url = require('url')
+
+const ledger = require('ledgerco')
+const fork = require('child_process').fork
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+var ledgercomm
+var eth
+
+// Keep a reference for dev mode
+let dev = false;
+if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath)) {
+  dev = true;
+}
+
+function createWindow() {
+
+  // setTimeout(() => {
+  //   console.log("L: ",ledger);
+  //   ledger.comm_node.list_async().then((deviceList) => {
+  //     console.log("D: ",deviceList.length);
+  //     if(deviceList.length > 0)
+  //           console.log("Connected: ", deviceList);
+  //     ledger.comm_node.create_async().then((comm) => {
+  //       console.log("C: ",comm)
+  //       ledgercomm = comm
+  //       console.log("LedgerComm: ", ledgercomm);
+  //       eth = new ledger.eth(ledgercomm);
+  //       console.log("ETH: ", eth);
+  //       let ethBip32 = "44'/60'/0'/";
+
+  //       for (let i = 0; i < 5; i++) {
+  //         eth.getAddress_async(ethBip32 + 0)
+  //           .then(
+  //           function (result) {
+  //             console.log("RESULT : ", result);
+  //             // event.sender.send('address', result);
+  //           })
+  //           .catch(
+  //           function (error) {
+  //             console.log("ERROR is: ", error);
+  //           });
+  //       }
+  //     }).fail((error) => console.log(error))
+  //   })
+  // }, 5000);
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1024, height: 768, show: false
+  });
+
+  // and load the index.html of the app.
+  let indexPath;
+  if (dev && process.argv.indexOf('--noDevServer') === -1) {
+    indexPath = url.format({
+      protocol: 'http:',
+      host: 'localhost:8080',
+      pathname: 'index.html',
+      slashes: true
+    });
+  } else {
+    indexPath = url.format({
+      protocol: 'file:',
+      pathname: path.join(__dirname, 'dist', 'index.html'),
+      slashes: true
+    });
+  }
+  mainWindow.loadURL(indexPath);
+
+  var ledgerWorker = fork(`${__dirname}/ledger-worker.js`)
+
+  ledgerWorker.on('message', (message) => {
+    if (message.connected && !ledgercomm) {
+      console.log("creating comm object")
+      ledger.comm_node.create_async().then((comm) => {
+        ledgercomm = comm
+        console.log("comm: ", comm);
+      }).fail((error) => console.log(error))
+    } else if (!message.connected && ledgercomm) {
+      ledgercomm.close_async()
+      ledgercomm = null
+    }
+  })
+
+
+
+  ipcMain.on('ledger', (event, arg) => {
+    if (arg.action == 'getAddress') {
+      let ethBip32 = "44'/60'/0'/";
+      eth = new ledger.eth(ledgercomm);
+            console.log("ETH: ", eth);
+      for (let i = 0; i < 5; i++) {
+        eth.getAddress_async(ethBip32 + i)
+          .then(
+          function (result) {
+            console.log("RESULT : ", result);
+            event.sender.send('address', result);
+          })
+          .catch(
+          function (error) {
+            console.log("ERROR is: ", error);
+          });
+      }
+    }
+  });
+
+  // Don't show until we are ready and loaded
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    // Open the DevTools automatically if developing
+    if (dev) {
+      mainWindow.webContents.openDevTools();
+    }
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import '../assets/css/App.css';
+// import '~/bootstrap/dist/css/boostrap.css'
+// import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+// import '../assets/css/bootstrap.min.css';
+// import "../assets/css/bootstrap-theme.min.css";
+import React, { Component } from 'react';
+var { ipcRenderer, remote } = require('electron');
+import {Panel, Nav, Navbar, Button} from 'react-bootstrap';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      eths: []
+    }
+
+    ipcRenderer.on('address', (event, arg) => {
+      console.log("Ledger Device ETH Address from main process: ", arg);
+      this.setState({eths:[...this.state.eths, arg.address]});
+    });
+  }
+  
+  onConnect() {
+    ipcRenderer.send('ledger', {
+      action: 'getAddress'
+    });
+  }
+
+  componentDidMount() {
+    // Listen for ledger device addresses message from main process
+
+  }
+
+
+  render() {
+    console.log("state: ", this.state);
+    return (
+      <div>
+      <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Electro-Ledger</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      
+    </Navbar>
+    <p>I am electron app, showing ledger eth addresses via REACT !</p>    
+    
+        <Panel header="ETH addresses" bsStyle="info">
+        <Button onClick={this.onConnect.bind(this)}>Connect</Button>
+       
+        <ul>
+        {
+          this.state.eths.map((eth,i) => {
+            return <li key={i}>{eth}</li>
+          })
+        }
+        </ul>
+      </Panel>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
